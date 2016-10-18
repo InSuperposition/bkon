fd548932d95a4813ec1a86e4a7076220e62ce046
Forrest Galloway <forrest.galloway@mosaik.com>
2016-10-18 15:11:03 -0500

    Add Options styles.

--- src/App/Options/index.js
+++ src/App/Options/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { container, top, bottom, option, icon } from './Options.css';
+import { container, top, bottom, option, text, icon, action, avatar } from './Options.css';
 import Gear from '../../assets/svgs/gear.svg';
 
 const Options = () => (
   <section className={container} >
     <div className={top} >
       <div className={option}>
-        <span>Bright Eyed Baby Happy Face</span>
-        <div>
-          <img role="presentation" />
+        <span className={text}>Bright Eyed Baby Happy Face</span>
+        <button className={action} >
+          <img src="https://placekitten.com/200/200" className={avatar} role="presentation" />
           <Gear className={icon} />
-        </div>
+        </button>
       </div>
     </div>
     <div className={bottom} />
