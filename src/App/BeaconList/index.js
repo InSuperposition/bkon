e247e8f5030f644543b948bf6072ad83a6a31073
Forrest Galloway <forrest.galloway@mosaik.com>
2016-10-20 14:26:31 -0500

    Improve layout.

--- src/App/BeaconList/index.js
+++ src/App/BeaconList/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { container, list, item } from './BeaconList.css';
+import { container, list, item, title, content, details, url, battery } from './BeaconList.css';
 import Toggle from '../../modules/Inputs/Toggle';
 import Checkbox from '../../modules/Inputs/Checkbox';
 
@@ -7,6 +7,12 @@ const Beacon = () => (
   <div className={container} >
     <Checkbox name={'test'} />
     <Toggle isActive />
+    <h2 className={title}>pHy1d</h2>
+    <div className={content}>
+      <span className={details}>Device Alias Here</span>
+      <span className={url}>https://destination.com/abcdefghijklmn1234567890</span>
+    </div>
+    <div className={battery}>Icon</div>
   </div>
 );
 
